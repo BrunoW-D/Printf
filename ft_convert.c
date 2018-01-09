5be5876658ea07f8a36703be13d95b3115188775
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2018-01-09 18:24:34 +0100

    Update ft_convert.c

--- ft_convert.c
+++ ft_convert.c
@@ -1,14 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_convert.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: bwang-do <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/04 14:46:49 by bwang-do          #+#    #+#             */
-/*   Updated: 2017/12/29 17:57:53 by bwang-do         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #include "ft.h"
 
@@ -22,75 +11,62 @@ int        is_type_flag(const char c)
         return (0);
 }
 
-int        init_flags(t_flags flags)
-{
-    int    i;
-
-    i = 0;
-    while (i < 5)
-    {
-        flags->options[i] = 0;
-    }
-    flags->modifier[0] = 0;
-    flags->modifier[1] = 0;
-    return (flags);
-}
-
-int     get_num(const char **format)
+int     get_num(const char **format, int *i)
 {
   int n;
   
   n = 0;
-  while (**format >= '0' && **format <= '9')
+  while ((*format)[*i] >= '0' && (*format)[*i] <= '9')
   {
       n *= 10;
-      n += **format;
-      *format++;
+      n += (*format)[*i];
+      *i++;
   }
   return (n);
 }
 
-void    get_modifier(t_flags *flags, const char **format)
+void    get_modifier(t_flags *flags, const char **format, int *i)
 { 
     (*flags)->modifier[0] = **format;
     (*flags)->modifier[1] = 0;
-    if ((**format == 'h' && **format  + 1 == 'h')
-            || (**format == 'l' && **format + 1 == 'l'))
+    if ((*format)[*i] == 'h' && (*format)[*i + 1] == 'h')
+            || ((*format)[*i] == 'l' && (*format)[*i + 1] == 'l'))
         (*flags)->modifier[1] = **format++;
-    *format++;
+    *i++;
 }
 
-char    *ft_convert(const char *format, va_list ap)
+char    *ft_get_flags(const char **format, va_list ap, t_flags flags)
 {
-    t_flags    flags;
+    int     i;
+    char    *str;
     
-    flags = init_flags(flags);
-    while (*format)
+    i = 0;
+    while ((*format)[i])
     {
-        if (*format == '#')
+        if ((*format)[i] == '#')
             flags->options[0] = 1;
-        else if (*format == '-')
+        else if ((*format)[i] == '-')
             flags->options[1] = 1;
-        else if (*format == '+')
+        else if ((*format)[i] == '+')
             flags->options[2] = 1;
-        else if (*format == ' ' && !flags->options[2])
+        else if ((*format)[i] == ' ' && !flags->options[2])
             flags->options[2] = 2;
-        else if (*format == '0' && !flags->options[1])
+        else if ((*format)[i] == '0' && !flags->options[1])
             flags->options[1] = 2;
-        else if (*format >= '1' && *format <= '9')
-            flags->options[3] = get_num(&format);
-        else if (*format == '.')
-            flags->options[4] = get_num(&(++format));
-        else if (*format == 'h' || *format == 'l'
-                || *format == 'j' || *format == 'z')
-            get_modifier(&flags, &format);
-        else if (*format == '%')
+        else if ((*format)[i] >= '1' && *format <= '9')
+            flags->options[3] = get_num(format, &i);
+        else if ((*format)[i] == '.')
+            flags->options[4] = get_num(format, &(++i));
+        else if ((*format)[i] == 'h' || (*format)[i] == 'l'
+                || (*format)[i] == 'j' || (*format)[i] == 'z')
+            get_modifier(&flags, format, &i);
+        else if ((*format)[i] == '%')
             return (ft_strdup("%"));
-        else if (is_type_flag(*format))
-            return (ft_print_controller(*format, ap, flags));
+        else if (is_type_flag((*format)[i]))
+            return ((str = ft_print_controller((*format)[i], ap, flags)));
         else
-            return (NULL);
-        format++;
+            return ((str = ft_strndup(*format, i)));
+        i++;
     }
-    return (NULL);
+    return ((str = ft_strndup(*format, i)));
 }
