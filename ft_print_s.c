70c780b61b3102ddda089aaf5f0d8b5266eeee15
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2018-01-13 16:48:44 +0100

    Update ft_print_s.c

--- ft_print_s.c
+++ ft_print_s.c
@@ -19,6 +19,10 @@ int		ft_print_s(va_list ap, t_flags *flags)
 	char	*ret;
 
 	str = va_arg(ap, char*);
+	if (flags->modifier[0] == flags->modifier[1] == 'l')
+		return (NULL);
+	else if (flags->modifier[0] == 'l')
+		return (ft_print_ss(ap, flags);
 	i = 0;
 	while (str[i] && i < flags->options[4])
 		i++;
