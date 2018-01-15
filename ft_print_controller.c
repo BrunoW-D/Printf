074a6e9dc65745deaca3ac9c5639702631617355
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2018-01-15 11:27:58 +0100

    Update ft_print_controller.c

--- ft_print_controller.c
+++ ft_print_controller.c
@@ -18,26 +18,25 @@ static char	*g_types = "sSpdDioOuUxXcC";
 void	init_p()
 {
 	g_p[0] = ft_print_s;
-	g_p[1] = ft_print_ss;
+	g_p[1] = ft_print_ls;
 	g_p[2] = ft_print_p;
 	g_p[3] = ft_print_d;
-	g_p[4] = ft_print_dd;
+	g_p[4] = ft_print_ld;
 	g_p[5] = ft_print_i;
 	g_p[6] = ft_print_o;
-	g_p[7] = ft_print_oo;
+	g_p[7] = ft_print_lo;
 	g_p[8] = ft_print_u;
-	g_p[9] = ft_print_uu;
+	g_p[9] = ft_print_lu;
 	g_p[10] = ft_print_x;
-	g_p[11] = ft_print_xx;
+	g_p[11] = ft_print_lx;
 	g_p[12] = ft_print_c;
-	g_p[13] = ft_print_cc;
+	g_p[13] = ft_print_lc;
 }
 
 char	*ft_print_controller(char c, va_list ap, t_data data)
 {
 	int		i;
 	char	*str;
-	int		len;
 
 	i = 0;
 	while (g_types[i])
@@ -46,60 +45,20 @@ char	*ft_print_controller(char c, va_list ap, t_data data)
 			str = g_p[i](ap, data->flags);
 		i++;
 	}
-	if ((len = ft_strlen(str)) < flags->options[3])
+	if (data->flags->options[1] == 2)
 	{
-		i = 0;
-		if (data->flags->options[1] == 2)
-		{
-			while (i < len)
-			{
-				if (i == BUFF_SIZE)
-				{
-					if ((str = ft_realloc_free(data->buff, str)) = NULL)
-						return (NULL);
-					ft_bzero(data->buff, BUFF_SIZE + 1);
-					i = 0;
-					len -= i;
-				}
-				data->buff[i++] = '0';
-			}
-			if ((str = ft_realloc_free(data->buff, str)) == NULL)
-				return (NULL);
-		}
-		else if (data->flags->options[1] == 1)
-		{
-			while (i < len)
-			{
-				if (i == BUFF_SIZE)
-				{
-					if ((str = ft_realloc(str, data->buff)) == NULL)
-						return (NULL);
-					ft_bzero(data->buff, BUFF_SIZE + 1);
-					i = 0;
-					len -= i;
-				}
-				data->buff[i++] = ' ';
-				if ((str = ft_realloc(str, data->buff)) == NULL)
-					return (NULL);
-			}
-		}
-		else if (data->flags->options[1] == 0)
-		{
-			while (i < len)
-			{
-				if (i == BUFF_SIZE)
-				{
-					if ((str = ft_realloc_free(data->buff, str)) == NULL)
-						return (NULL);
-					ft_bzero(data->buff, BUFF_SIZE + 1);
-					i = 0;
-					len -= i;
-				}
-				data->buff[i++] = ' ';
-			}
-			if ((str = ft_realloc_free(data->buff, str)) == NULL)
-				return (NULL);
-		}
+		if ((str = ft_realloc_free(ft_nchar('0', flags->options[3]), str)) == NULL)
+			return (NULL);
+	}
+	else if (data->flags->options[1] == 1)
+	{
+		if ((str = ft_realloc(str, ft_nchar(' ', flags->options[3]))) == NULL)
+			return (NULL);
+	}
+	else if (data->flags->options[1] == 0)
+	{
+		if ((str = ft_realloc_free(ft_nchar(' ', flags->options[3]), str)) == NULL)
+			return (NULL);
 	}
 	return (str);
 }
