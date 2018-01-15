6d4cdd38c927d830e38e65d15415ea89b2d9758d
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2018-01-15 12:21:22 +0100

    Create ft_print_x.c

---
+++ ft_print_x.c
@@ -0,0 +1,14 @@
+#include "ft.h"
+
+int		ft_print_x(va_list ap, t_flags *flags)
+{
+	unsigned int	x;
+	char			*ret;
+
+	x = va_arg(ap, unsigned int);
+	ret = ft_base(o, 16);
+	if (flags->options[0])
+		if ((ret = ft_realloc_free("0x", ret)) == NULL)
+			return (NULL);
+	return (ret);
+}
