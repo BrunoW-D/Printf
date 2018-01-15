0365235b0fa4fc7d0ce20a6f695e66e359aa5585
Bruno Wang-Domingo <bwangdomingo@gmail.com>
2018-01-15 11:19:17 +0100

    Update ft_print_c.c

--- ft_print_c.c
+++ ft_print_c.c
@@ -6,8 +6,5 @@ int		ft_print_c(va_list ap, t_flags *flags)
 	char	*ret;
 
 	(char)c = va_arg(ap, int);
-	ret = ft_strnew(flags->options[3] + 2);
-	ret = ft_strcpy(ft_nchar(' ', flags->options[3]));
-	ret = ft_strcat(ret, &c);
 	return (ret);
 }
